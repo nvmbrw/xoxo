27b60de20c7edb5087037c3ae3781dd3f35c35e7
November Williams <nnwilliams8@gmail.com>
2018-07-05 13:01:13 -0400

    generic commit message--command was run by script

--- game/index.js
+++ game/index.js
@@ -1,6 +1,58 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const MOVE = 'MOVE'
+
+const turnReducer = (turn = 'X', action) => {
+  if (action.type === MOVE) {
+    return turn === 'X' ? 'O' : 'X'
+  }
+}
+
+const boardReducer = (board = Map(), action) => {}
+
+export default function gameReducer(
+  prevState = { board: Map(), turn: 'O' },
+  action
+) {
+  switch (action.type) {
+    case 'MOVE':
+      let newBoard = {
+        board: prevState.board.setIn(action.position, action.player),
+        turn: action.player === 'X' ? 'O' : 'X',
+      }
+      return newBoard
+    default:
+      return prevState
+  }
+}
+
+export function move(player, coords) {
+  return { type: 'MOVE', position: coords, player: player }
+}
+
+function streak(board, firstCoord, ...remainingCoords) {
+  const value = board.getIn(firstCoord)
+
+  for (let i = 0; i < remainingCoords.length; i++) {
+    if (board.getIn(remainingCoords[i]) !== value) return undefined
+  }
+  return value
+}
+
+export function winner(board) {
+  for (let i = 0; i < 3; i++) {
+    let strk = streak(board, [i, 0], [i, 1], [i, 2])
+    if (strk) return strk
+  }
+
+  for (let i = 0; i < 3; i++) {
+    let strk = streak(board, [0, i], [1, i], [2, i])
+    if (strk) return strk
+  }
+  let strk = streak(board, [0, 0], [1, 1], [2, 2])
+  if (strk) {
+    return strk
+  } else {
+    return streak(board, [2, 0], [1, 1], [0, 2])
+  }
 }
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import inquirer from 'inquirer'
 
-import gameReducer, {move} from './game'
-import {createStore} from 'redux'
+import gameReducer, { move } from './game'
+import { createStore } from 'redux'
+
+const game = createStore(gameReducer)
 
 const printBoard = () => {
-  const {board} = game.getState()
+  const { board } = game.getState()
   for (let r = 0; r != 3; ++r) {
     for (let c = 0; c != 3; ++c) {
       process.stdout.write(board.getIn([r, c], '_'))
@@ -14,20 +16,20 @@ const printBoard = () => {
 }
 
 const getInput = player => async () => {
-  const {turn} = game.getState()  
+  const { turn } = game.getState()
   if (turn !== player) return
-  const ans = await inquirer.prompt([{
-    type: 'input',
-    name: 'coord',
-    message: `${turn}'s move (row,col):`
-  }])
-  const [row=0, col=0] = ans.coord.split(/[,\s+]/).map(x => +x)
+  const ans = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'coord',
+      message: `${turn}'s move (row,col):`,
+    },
+  ])
+  const [row = 0, col = 0] = ans.coord.split(/[,\s+]/).map(x => +x)
   game.dispatch(move(turn, [row, col]))
 }
 
 // Create the store
-const game = createStore(gameReducer)
-
 // Debug: Print the state
 // game.subscribe(() => console.log(game.getState()))
 
